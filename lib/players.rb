90d36121be342119aa2f5353abe2eb4cf7b6d7a1
Alexandru <rammazzoti2000@gmail.com>
2020-03-17 14:03:56 +0100

    players changes

--- lib/players.rb
+++ lib/players.rb
@@ -1,8 +1,8 @@
 require_relative '../bin/main.rb'
-require_relative 'position.rb'
+require_relative '../lib/position.rb'
 
 class Player
-    attr_accessor :player_1, :player_2 , @current_player  
+    attr_accessor :player_1, :player_2 , :current_player  
 
     def initialize(player_1,player_2)
         @player_1 = player_1
@@ -11,12 +11,16 @@ class Player
     end
 
     def switch_turn
+        $count += 1
+        #position_num
         @current_player == @player_1 ? @current_player = @player_2 : @current_player = @player_1
+        
     end
 
 
 end
 
 players = Player.new($layer_1, $player_2)
-players.player_1
-players.player_2
+#puts players.player_1
+#puts players.player_2
+puts players.switch_turn
