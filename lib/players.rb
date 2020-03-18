606c74907d65b328fdd8ba64f8ac3c8a0782149e
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-19 02:44:55 +0530

    fix more rubocop errors

--- lib/players.rb
+++ lib/players.rb
@@ -1,12 +1,12 @@
 class Player
-  attr_reader :player1, :player_2, :player1_drink, :player_2_drink
+  attr_reader :player1, :player2, :player1_drink, :player2_drink
   attr_accessor :current_player, :current_drink
 
-  def initialize(player1, player_2, player1_drink, player_2_drink)
+  def initialize(player1, player2, player1_drink, player2_drink)
     @player1 = player1
-    @player2 = player_2
+    @player2 = player2
     @player1_drink = player1_drink
-    @player2_drink = player_2_drink
+    @player2_drink = player2_drink
     @current_drink = @player2_drink
   end
 
@@ -20,34 +20,42 @@ class Player
   ]
 
   def self.board
+    puts
     @board
   end
 
   def self.board=(num)
+    puts
     @board = num
   end
 
   def self.current_player
+    puts
     @current_player
   end
 
   def self.current_player=(player)
+    puts
     @current_player = player
   end
 
   def self.current_drink=(*)
+    puts
     @current_drink = @player2_drink
   end
 
   def self.winning_positions
+    puts
     @winning_positions
   end
 
   def self.count
+    puts
     @count
   end
 
   def self.count=(num)
+    puts
     @count = num
   end
 
