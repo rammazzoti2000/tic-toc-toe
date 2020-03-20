7a633a8b7ab5bed64630afedb726ce0ae3c760ce
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-20 20:06:53 +0530

    add Rspec test initialize method

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -1,3 +1,21 @@
+# spec/players_spec.rb
 
+require 'players'
+
+describe Player do
+    let(:player) { Player.new('player1', 'player2', 'player1_drink' , 'player2_drink') }
+
+    describe '#initialize' do
+        it 'should take 4 arguments' do
+            player
+        end
+
+        it 'returns an ArgumentError if the Player is initialized with no arguments or more than 4 arguments' do
+            expect {Player.new}.to raise_error(ArgumentError)
+        end
+    end
+
+
+end
 
 
